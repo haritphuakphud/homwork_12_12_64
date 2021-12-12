962e5979cc9256594e9f13cfb7861ade30a9865a
guess_number <mm0625257659@gmail.com>
2021-12-12 23:20:52 +0700

    commit

--- lib/homwork.dart
+++ lib/homwork.dart
@@ -1,22 +1,37 @@
+
 import 'dart:math';
 import 'dart:io';
 void main() {
-  var i=0;
+  var i = 0;
   var r = Random();
-  var answer =r.nextInt(100);
+  var answer = r.nextInt(100 + 1);
+  var count = 1;
   while (true) {
     stdout.write('please guess the number: ');
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!);
-    if(guess == answer){
-      print('Congratulations');
-      break;
-    }
-    else{
-      print('Thy Again');
+    if (guess == null) {
       i++;
     }
+    else {
+      if (guess == answer) {
+        print('$guess is CORRECT❤');
+        print('Total guesses is $count.');
+        break;
+      }
+      else {
+        if (guess > answer) {
+          print('$guess is TOO HIGH!');
+          count++;
+          i++;
+        }
 
+        else {
+          print('$guess is TOO LOW!');
+          count++;
+          i++;
+        }
+      }
+    }
   }
-
 }
